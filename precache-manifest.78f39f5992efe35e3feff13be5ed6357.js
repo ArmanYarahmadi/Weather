5dc71e53af19dd97a50d4a048600997e4450ab70
ArmanYarahmadi <arm1377arman@gmail.com>
2020-06-04 09:29:23 +0430

    Updates

---
+++ precache-manifest.78f39f5992efe35e3feff13be5ed6357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9a4986f453930c50af8a69f6269c76",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "92b892da7b611acf2559",
+    "url": "/Weather/static/css/main.84681c56.chunk.css"
+  },
+  {
+    "revision": "31676ba741e564c055a1",
+    "url": "/Weather/static/js/2.619b632d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.619b632d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b892da7b611acf2559",
+    "url": "/Weather/static/js/main.076a4a6f.chunk.js"
+  },
+  {
+    "revision": "dad2ac99047398a42dce",
+    "url": "/Weather/static/js/runtime-main.32d3e3b4.js"
+  }
+]);
